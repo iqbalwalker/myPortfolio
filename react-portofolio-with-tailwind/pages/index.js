8abf7a615d48ddb013409bf5b1bde529a1533aba
iqbalwalker <alexarjohns@gmail.com>
2023-10-18 15:31:54 +0300

    escaped with &#39;

--- react-portofolio-with-tailwind/pages/index.js
+++ react-portofolio-with-tailwind/pages/index.js
@@ -60,7 +60,7 @@ export default function Home() {
               Hi there,
             </h2>
             <h2 className="text-5xl py-2 text-blue-600 font-medium dark:text-teal-400 md:text-6xl">
-              I'm Alexa John, 
+              I&#39;m Alexa John, 
             </h2>
             <h3 className="text-teal-600 text-2xl py-2 dark:text-yellow-600 md:text-3xl">
               a Software Engineer
@@ -88,7 +88,7 @@ export default function Home() {
             <h3 className="text-3xl py-1 text-center dark:text-white ">Services Offered</h3>
             <p className="text-md py-2 leading-8 text-gray-800 text-center dark:text-gray-200">
               Since the beginning of my journey as a freelance 
-              developer, I've done remote work for
+              developer, I&#39;ve done remote work for
               <span className="text-teal-500"> agencies </span>
               consulted for <span className="text-teal-500">startups </span>
               and collaborated with talanted people to create digital products
@@ -120,7 +120,7 @@ export default function Home() {
                 Code your dream project
               </h3>
               <p className="py-2">
-                Do you have an idea for your next great website? Let's make it a
+                Do you have an idea for your next great website? Let&#39;s make it a
                 reality.
               </p>
               <h4 className="py-4 text-teal-600">Frameworks that I am well versed with</h4>
@@ -148,7 +148,7 @@ export default function Home() {
             <h3 className="text-3xl py-1 text-center dark:text-white ">Portfolio</h3>
             <p className="text-lg py-2 leading-8 text-gray-800 text-center dark:text-gray-200">
               Since the beginning of my journey as a freelance designer and
-              developer, I've done remote work for<br></br>
+              developer, I&#39;ve done remote work for<br></br>
               <span className="text-teal-500 text-lg"> agencies </span>
               consulted for <span className="text-teal-500">startups </span>
               and collaborated with talanted people <br></br> to create digital products
